71cc34a1f123a5ea2ce1806564d1b510d9ef6b89
apul8377 <apulvers@my.chemeketa.edu>
2020-06-07 14:49:02 -0700

    practice

---
+++ 6.7.2020.js
@@ -0,0 +1,29 @@
+let total = 0;
+for (let currentNumber = 1; currentNumber <= 5; currentNumber += 1) {
+  total += currentNumber;
+}
+total
+
+
+
+//_______________first loop around___________
+//currenNumber = 1
+//currentNumber = currentNumber + 1 = 2
+//total = 0
+// total = total + currentNumber
+//total = 0 + 2 = 1
+//______________second loop around____________
+//currentNumber = 2
+//currentNumber = currentNumber + 1 = 2
+//total = 1 + 2 = 3
+//______________third loop around____________
+//currentNumber = 3
+//currentNumber = currentNumber + 1 = 4
+//total = 3 + 3 = 6
+//______________fourth loop around____________
+//currentNumber = 4
+//currentNumber = currentNumber + 1 = 5
+//total = 6 + 4 = 10
+//____________fifth loop around_______________
+//currentNumber = 5
+//total = 10 + 5 = 15 
